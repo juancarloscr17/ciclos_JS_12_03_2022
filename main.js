d15a35f59ae1acf55530885fb4e01a0f29950429
juancarloscr17 <juankcr17@gmail.com>
2022-03-12 09:54:12 -0500

    ciclo for

---
+++ main.js
@@ -0,0 +1,14 @@
+for (let i= 0; i < 5; i++) {
+   console.log(`Ciclo normal ${i}`);
+}
+
+let x =0;
+for(x;true;){
+    if(x<5){
+        console.log(`Ciclo personalizado ${x}`);
+    }
+    else{
+        break;
+    }
+    x++
+}
